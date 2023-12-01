7f1c4c317b6591171b0613570eb47f0431c7c4f7
hockeyiscool19 <jordaneisenman@gmail.com>
2023-12-01 18:13:56 -0500

    viz

--- src/viz.py
+++ src/viz.py
@@ -5,9 +5,7 @@ import rasterio
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-file = rasterio.open("gis/USGS_13_n39w107_20220331.tif")
-dataset = file.read()
+dataset = np.load("gis/map.npy")
 print(dataset.shape)
 print(dataset[0])
 plt.imshow(dataset[0], cmap='Spectral')
